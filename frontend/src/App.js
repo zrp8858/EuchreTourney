ade1db95afc851d9a52f29fbf50039fa4ef233c3
AJBruno23 <ajbru23@gmail.com>
2025-09-30 21:49:12 -0400

    feat: leaderboard and matchup flexboxes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,48 @@
-import './App.css';
+import React from 'react'
+import { Box, Typography, Stack, Container } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      Euchre Tournament Scores
-    </div>
+    <Container>
+      <Box sx={{ textAlign: 'center', margin: '20px' }}>
+        <Typography variant="h3">Euchre Tournament Scores</Typography>
+      </Box>
+      <Stack 
+        direction="row"
+        spacing={25}
+        sx={{
+          width: '100%',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            border: '2px solid #000',
+            borderRadius: 2,
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <Typography variant="h6">Leaderboard</Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            border: '2px solid #000',
+            borderRadius: 2,
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <Typography variant="h6">Matchups</Typography>
+        </Box>
+      </Stack>
+    </Container>
   );
 }
 
